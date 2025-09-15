e3ac56afc41e06dff32f0a86032c79af2a5f7a3c
caromalpop <reabetswemasima55@gmail.com>
2025-09-15 21:31:47 +0200

    Add files via upload

---
+++ Randomised_flavours_of_IC.py
@@ -0,0 +1,7 @@
+import random
+
+flavours = ['Vanilla', 'Chocolate', 'Strawberry', 'Mint', 'Cookie Dough', 'Pistachio', 'Mango', 'Coffee']
+
+random_flavour = random.choice(flavours)
+
+print("Randomly selected ice cream flavour:", random_flavour)
