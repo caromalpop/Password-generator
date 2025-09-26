98b062bb0728c784647b587f358922e0121097a8
caromalpop <reabetswemasima55@gmail.com>
2025-09-26 19:20:35 +0200

    Add files via upload

---
+++ Grocery_list.py
@@ -0,0 +1,12 @@
+#Creating a grocery list:
+grocery_list = ["apples", "bananas", "carrots", "dates"]
+print("Initial grocery list:", grocery_list)
+#Adding an item to the list:
+grocery_list.append("eggs")
+print("After adding eggs:", grocery_list)
+#Removing an item from the list:
+grocery_list.remove("carrots")
+print("After removing carrots:", grocery_list)
+#Sorting the list:
+grocery_list.sort()
+print("Sorted grocery list:", grocery_list)
